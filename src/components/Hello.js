40815c82d5dd03d5f83bad49ce5c99fd2068f9ae
Lorenzo dela Cruz <ldelacruz@mmsu.edu.ph>
2017-12-15 08:37:34 +0800

    Initial Commit

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+class Home extends React.Component {
+    render () {
+      return (
+          <div className="container">
+              <Nav/>
+          </div>
+      );
+    }
+}
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require("webpack");
+var path = require("path");
+
+var dist = path.resolve(__dirname,"./dist");
+
+var src = path.resolve(__dirname,"src");
+
+/*module.exports={
+    entry: src+"/app/index.js",
+    output: {
+      ouput: dist+"/app",
+      filename: "bundel.js",
+      publicPath:"/app/"
+    },
+    modules:{
+      rules:[
+        {
+          test:/\.js?/,
+          include: src,
+          loader:"babel-loader",
+          query:{
+            presets:["react","es2015","stage-2"]
+          }
+        },
+      ],
+    }
+
+}*/
+module.exports={
+    entry:{app:[
+        './src/app/index.js',
+      ]},
+    output:{
+      path:dist+"/app/",
+      filename:'bundel.js',
+      publicPath:"/app/"
+    },
+    module:{
+        rules:[
+          {
+            test:/\.js?/,
+            include: src,
+            loader:"babel-loader",
+            query:{
+              presets:["react","es2015","stage-2"]
+            }
+          }
+        ],//end of rules array
+
+    },//end of module object
+
+
+
+};
